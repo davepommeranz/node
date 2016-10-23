f1c701bff3bd64fff535d2b2c02d043facea90ab
Dave Pommeranz <davepommeranz@gmail.com>
2016-10-22 16:27:42 -1000

    userlist route added and pulling data from mongodb

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,15 @@ router.get('/contact', function(req, res){
   });
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
 module.exports = router;
